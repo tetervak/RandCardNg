ebb4dbca12cc0325c8d6fb1acb2efba6be46a8e7
Alex Tetervak <tetervak@sheridancollege.ca>
2019-10-28 17:17:05 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RandCardNg';
+  title = 'Random Card Ng';
+  card = new RandomCard();
 }
+
+class RandomCard {
+  // these arrays are used to generate the random card names
+  static SUITS = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+  static FACES = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+    'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+
+  public cardName: string;
+  public imgSrc: string;
+  // generate a random card info
+  constructor() {
+    this.reset();
+  }
+  public reset() {
+    // the random number  0..51
+    const rand = Math.floor(52 * Math.random());
+    // the card number 1..52 is matching the image file name
+    const cardNumber = rand + 1;
+    // the name of the card determined via the math trick
+    this.cardName
+      = RandomCard.FACES[rand % 13]
+      + ' of '
+      + RandomCard.SUITS[Math.floor(rand / 13)];
+    // generate the image path
+    this.imgSrc = `assets/images/cards/${cardNumber}.png`;
+  }
+}
+
