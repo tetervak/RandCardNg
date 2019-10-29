00ffb6539a581e03f95b456738fed0e6a03029ed
Alex Tetervak <tetervak@sheridancollege.ca>
2019-10-29 02:30:22 -0400

    moved RandomCard class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RandomCard} from './random-card';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Random Card Ng';
-  public readonly card;
-  private readonly nextCard: RandomCard;
+  public card: RandomCard;
+  private nextCard: RandomCard;
   private nextImage: HTMLImageElement;
   constructor() {
     this.card = new RandomCard();
@@ -23,37 +24,6 @@ export class AppComponent {
     this.card.copy(this.nextCard);
     this.nextCard.reset();
     this.preload();
-  }
-}
-
-class RandomCard {
-  // these arrays are used to generate the random card names
-  static SUITS = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-  static FACES = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-    'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
-
-  public cardName: string;
-  public imgSrc: string;
-  // generate a random card info
-  constructor() {
-    this.reset();
-  }
-  public reset() {
-    // the random number  0..51
-    const rand = Math.floor(52 * Math.random());
-    // the card number 1..52 is matching the image file name
-    const cardNumber = rand + 1;
-    // the name of the card determined via the math trick
-    this.cardName
-      = RandomCard.FACES[rand % 13]
-      + ' of '
-      + RandomCard.SUITS[Math.floor(rand / 13)];
-    // generate the image path
-    this.imgSrc = `assets/images/cards/${cardNumber}.png`;
-  }
-  copy(card: RandomCard) {
-    this.cardName = card.cardName;
-    this.imgSrc = card.imgSrc;
   }
 }
 
---
+++ src/app/random-card.ts
@@ -0,0 +1,30 @@
+export class RandomCard {
+  // these arrays are used to generate the random card names
+  static SUITS = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+  static FACES = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+    'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+
+  public cardName: string;
+  public imgSrc: string;
+  // generate a random card info
+  constructor() {
+    this.reset();
+  }
+  public reset() {
+    // the random number  0..51
+    const rand = Math.floor(52 * Math.random());
+    // the card number 1..52 is matching the image file name
+    const cardNumber = rand + 1;
+    // the name of the card determined via the math trick
+    this.cardName
+      = RandomCard.FACES[rand % 13]
+      + ' of '
+      + RandomCard.SUITS[Math.floor(rand / 13)];
+    // generate the image path
+    this.imgSrc = `assets/images/cards/${cardNumber}.png`;
+  }
+  copy(card: RandomCard) {
+    this.cardName = card.cardName;
+    this.imgSrc = card.imgSrc;
+  }
+}
