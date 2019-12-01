1a101673e02b5ecece51629352659c40e37123c8
Alex Tetervak <tetervak@sheridancollege.ca>
2019-11-30 19:03:16 -0500

    made the footer folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FooterComponent} from './footer.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
